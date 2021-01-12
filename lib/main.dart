305af7c85602fde24b87c42b3b877f9dc244b8fa
rayhan <syahrayhanid@gmail.com>
2021-01-13 05:43:24 +0700

    add description

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "wisata Bandung",
+      debugShowCheckedModeBanner: false,
+      title: 'Wisata Bandung',
       theme: ThemeData.dark(),
       home: DetailScreen(),
     );
@@ -14,20 +17,31 @@ class MyApp extends StatelessWidget {
 }
 
 class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column (
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.arrow_back),
+        actions: <Widget>[
+          Icon(Icons.bookmarks),
+          Text("      ")
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
+          Container(  
             margin: EdgeInsets.only(top: 40),
             child: Text(
               'Farm House Lembang',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 30.0,
-                fontWeight: FontWeight.bold  
-              ),
+                fontWeight: FontWeight.bold
+              ),           
             ),
           ),
           Container(
@@ -35,30 +49,34 @@ class DetailScreen extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                Column(
-                  children: [
-                    Icon(Icons.calendar_today),
-                    SizedBox(height: 8.0),
-                    Text('Open Everyday')
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.circle),
-                    SizedBox(height: 8.0),
-                    Text("09:00 - 20:00")
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Icon(Icons.person_pin_circle),
-                    SizedBox(height: 8.0,),
-                    Text("Rp. 25.000")
-                  ],
-                )
+                Column(children: <Widget>[
+                  Icon(Icons.calendar_today),
+                  SizedBox(height: 8.0,),
+                  Text("Open Everiday")
+                ],),
+                Column(children: [
+                  Icon(Icons.schedule),
+                  SizedBox(height: 8.0,),
+                  Text('09:00 - 20:00')
+                ],),
+                Column(children: [
+                  Icon(Icons.attach_money_outlined),
+                  SizedBox(height: 8.0,),
+                  Text("Rp. 25.000")
+                ],)
               ],
             ),
-          )
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ),
         ],
       ),
     );
